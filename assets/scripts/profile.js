bbbf8df1ff4b93a5b1d88529c040377e0445491f
SKR0411 <mr.sujan.kumar.roy@gmail.com>
2025-12-04 11:09:42 +0530

    Last Sync: 2025-12-04 11:09 (Mobile)

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -4,6 +4,9 @@ const comProfileBtn = document.getElementById("com-profile-btn");
 
 const userName = document.getElementById("username");
 const email = document.getElementById("email");
+const userId = document.getElementById("userid");
+
+let isExist = false;
 
 function sync() {
     
@@ -13,19 +16,31 @@ function sync() {
     if (user) {
         userName.textContent = user.name;
         email.textContent = '' + user.email;
+        userId.textContent = user.id;
         
+        isExist = true;
         loginSignupBtn.style.display = "none";
         logoutBtn.style.display = "block";
         comProfileBtn.style.display = "block";
     } else {
-        userName.textContent = "your Name";
+        userName.textContent = "Your Name";
         email.textContent = "yourname@example.com";
+        userId.textContent = "User ID"
         
+        isExist = false;
         loginSignupBtn.style.display = "block";
         logoutBtn.style.display = "none";
         comProfileBtn.style.display = "none";
     }
         
+}
+
+function fullProfile() {
+    if (isExist) {
+        window.location.href='profile_complition_form.html'
+    } else {
+        window.location.href='login_signup.html'
+    }
 }
 
 logoutBtn.addEventListener("click", () => {
--- assets/scripts/profile_complition_form.js
+++ assets/scripts/profile_complition_form.js
@@ -16,7 +16,7 @@ function load() {
     
     input[0].value = user.name;
     input[1].value = user.email;
-    input[2].value = user.age;
+    input[2].value = user.age ? user.age : null;
     input[3].value = user.fv_subject ? user.fv_subject : null;
     input[4].value = user.stream ? user.stream : null;
     inputEdu.value = user.standered ? user.standered : "";

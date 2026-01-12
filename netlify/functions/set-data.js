25ef33db398128a7c63c93082860d22c04a06461
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-01-12 22:46:50 +0530

    typing mistake

--- netlify/functions/set-data.js
+++ netlify/functions/set-data.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_DATABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabaseClient = createClient(process.env.SUPABASE_DATABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default async (request) => {
     

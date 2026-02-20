bdfd9a8794574012f65142b7fdfd188e91e3e23e
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-02-20 22:27:51 +0530

    fixed small error in fav and history page

--- assets/scripts/favourite.js
+++ assets/scripts/favourite.js
@@ -63,43 +63,45 @@
             return;
         }
 
-        const res = await fetch('https://knowlet.in/.netlify/functions/get-favs', {
-            method: 'POST',
-            header: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                user_id: user.id
-            })
-        });
+        try {
+            const res = await fetch('https://knowlet.in/.netlify/functions/get-favs', {
+                method: 'POST',
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user_id: user.id
+                })
+            });
+        
+            const { data: favs, error } = await res.json();
+    
+            if (error) {
+                favouritesList.innerHTML = '<li class="empty-message">Failed to fetch data, try to refresh the page!</li>';
+                return;
+            }
 
-        const { data: favs, error } = await res.json();
-
-        if (error) {
-            favouritesList.innerHTML = '<li class="empty-message">Failed to fetch history, try to refresh the page!</li>';
-            return;
+            favs.sort((a, b) => b.interactions_time.faved_at.localeCompare(a.interactions_time.faved_at));
+    
+            favouritesList.innerHTML = ''; // Clear loading message
+    
+            if (favs.length === 0) {
+                favouritesList.innerHTML = '<li class="empty-message">You have no pages marked as favourite yet. Go to a unit page and click "Add to Favourites"!</li>';
+                return;
+            }
+    
+            favs.forEach(item => {
+                const title = item.page_title ? item.page_title : generateTitleFromURL(item.page_id);
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    <a href="${item.page_id}" title="Go to ${title}">${title}</a>
+                `;
+                favouritesList.appendChild(listItem);
+            });
+        } catch(err) {
+            console.error(err);
+            favouritesList.innerHTML = '<li class="empty-message">Failed to fetch data</li>';
         }
-
-        console.log(favs)
-        console.log(JSON.parse(localStorage.getItem("unit_page_favourites") || '[]'))
-
-        favs.sort((a, b) => b.interactions_time.faved_at.localeCompare(a.interactions_time.faved_at));
-
-        favouritesList.innerHTML = ''; // Clear loading message
-
-        if (favs.length === 0) {
-            favouritesList.innerHTML = '<li class="empty-message">You have no pages marked as favourite yet. Go to a unit page and click "Add to Favourites"!</li>';
-            return;
-        }
-
-        favs.forEach(item => {
-            const title = item.page_title ? item.page_title : generateTitleFromURL(item.page_id);
-            const listItem = document.createElement('li');
-            listItem.innerHTML = `
-                <a href="${item.page_id}" title="Go to ${title}">${title}</a>
-            `;
-            favouritesList.appendChild(listItem);
-        });
     }
 
     renderFavourites();
--- assets/scripts/history.js
+++ assets/scripts/history.js
@@ -62,61 +62,67 @@
             return;
         }
 
-        const res = await fetch('https://knowlet.in/.netlify/functions/get-history', {
-            method: 'POST',
-            header: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                user_id: user.id
-            })
-        });
-
-        const { data, error } = await res.json();
-        if (error) {
-            historyList.innerHTML = '<li class="empty-message">Failed to fetch history, try to refresh the page!</li>';
-            return;
-        }
-
-        let history = [];
-        data.forEach((item) => {
-            JSON.parse(item.visit_time).forEach((ts) => {
-                if (ts) {
-                    history.push({
-                        url: item.page_id,
-                        title: item.page_title,
-                        timestamp: ts
-                    })
-                }
+        try {
+            const res = await fetch('https://knowlet.in/.netlify/functions/get-history', {
+                method: 'POST',
+                header: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user_id: user.id
+                })
             });
-        });
+    
+            const { data, error } = await res.json();
 
-        history.sort((a, b) => b.timestamp.localeCompare(a.timestamp));
-
-        console.log(history)
-        
-        historyList.innerHTML = ''; // Clear loading message
-
-        if (history.length === 0) {
-            historyList.innerHTML = '<li class="empty-message">Your visit history is empty. Start exploring the unit pages!</li>';
-            return;
-        }
-
-        let tempUrl, tempTa;
-        history.forEach(item => {
-            const time = timeAgo(new Date(item.timestamp).getTime());
-            if (!(tempUrl === item.url && tempTa === time)) {
-                const listItem = document.createElement('li');
-                const title = item.title || generateTitleFromURL(item.url)
-                listItem.innerHTML = `
+            if (error) {
+                historyList.innerHTML = '<li class="empty-message">Failed to fetch history, try to refresh the page!</li>';
+                return;
+            }
+    
+            let history = [];
+            data.forEach((item) => {
+                JSON.parse(item.visit_time).forEach((ts) => {
+                    if (ts) {
+                        history.push({
+                            url: item.page_id,
+                            title: item.page_title,
+                            timestamp: ts
+                        })
+                    }
+                });
+            });
+    
+            history.sort((a, b) => b.timestamp.localeCompare(a.timestamp));
+    
+            console.log(history)
+            
+            historyList.innerHTML = ''; // Clear loading message
+    
+            if (history.length === 0) {
+                historyList.innerHTML = '<li class="empty-message">Your visit history is empty. Start exploring the unit pages!</li>';
+                return;
+            }
+    
+            let tempUrl, tempTa;
+            history.forEach(item => {
+                const time = timeAgo(new Date(item.timestamp).getTime());
+                if (!(tempUrl === item.url && tempTa === time)) {
+                    const listItem = document.createElement('li');
+                    const title = item.title || generateTitleFromURL(item.url)
+                    listItem.innerHTML = `
     <a href="${item.url}" title="Go to ${title}">${title}</a>
     <span class="timestamp">${time}</span>
-                `;
-                historyList.appendChild(listItem);
-            }
-            tempUrl = item.url;
-            tempTa = time;
-        });
+                    `;
+                    historyList.appendChild(listItem);
+                }
+                tempUrl = item.url;
+                tempTa = time;
+            });
+        } catch(err) {
+            console.error(err);
+            historyList.innerHTML = '<li class="empty-message">Failed to fetch data</li>';
+        }
     }
     
     renderHistory();

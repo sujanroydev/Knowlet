0e48cd72724db5808415684f89b47d489925243f
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-04-13 12:57:37 +0530

    added event listener in serviceworker for push notification

--- sw.js
+++ sw.js
@@ -115,3 +115,20 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
+self.addEventListener('push', event => {
+    const data = event.data.json();
+
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: '/assets/icons/android-chrome-192x192.png'
+    });
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});

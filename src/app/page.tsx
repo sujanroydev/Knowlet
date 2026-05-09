5d59073aeb6af0c24018bbcd33d3b46dafb1f0d0
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-09 09:56:40 +0530

    feat(home); add search bar to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/app/components/home/Hero";
+import SearchBar from "./components/home/SearchBar";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <SearchBar />
     </div>
   );
 }

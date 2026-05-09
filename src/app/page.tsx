4f67f9606ed8c4880905ad1f2ced19cb15162f2b
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-09 10:26:16 +0530

    feat(home): add library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/home/Hero";
 import SearchBar from "./components/home/SearchBar";
+import Library from "./components/home/Library";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <SearchBar />
+      <Library />
     </div>
   );
 }

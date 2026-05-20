8a10b6798f403a606bf8464c02142ead356dc046
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-20 21:09:56 +0530

    feat(api): parse path and upsert dynamic resource hierarchy

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { title, description, content, target, type, slug, path } =
+    const { title, description, content, target, type, slug, path }: Resource =
       await req.json();
 
     if (
@@ -33,11 +33,82 @@ export async function POST(req: NextRequest) {
 
     const resource = { title, description, content, target, type, slug, path };
 
+    const parts = path.split("/");
+
+    const levelSlug = parts[0];
+    const subjectSlug = parts[1];
+    const paperSlug = subjectSlug.startsWith("semester") ? parts[2] : null;
+
     const db = await connectDb();
 
-    const { data, error } = await db.from("resources").insert(resource);
+    // console.log(path.split("/"));
 
-    if (error) throw new Error(error.message);
+    let { data: level, error: levelError } = await db
+      .from("levels")
+      .select("id")
+      .eq("slug", path.split("/")[0])
+      .maybeSingle();
+
+    if (!level) {
+      const levelRow = await db
+        .from("levels")
+        .insert({
+          title: levelSlug.split("-").join(" ").toUpperCase(),
+          number: levelSlug.split("-")[1],
+          slug: levelSlug,
+          path: `${levelSlug}`,
+        })
+        .select()
+        .single();
+
+      level = levelRow.data;
+    }
+
+    // console.log("level", level);
+
+    let { data: subject, error: subjectError } = await db
+      .from("subjects")
+      .select("id")
+      .eq("slug", path.split("/")[1])
+      .eq("level_id", level?.id)
+      .maybeSingle();
+
+    if (!subject) {
+      const levelRow = await db
+        .from("subjects")
+        .insert({
+          title: levelSlug.split("-").join(" ").toUpperCase(),
+          number: levelSlug.split("-")[1],
+          slug: levelSlug,
+          path: `${levelSlug}`,
+        })
+        .select()
+        .single();
+
+      level = levelRow.data;
+    }
+
+    // console.log("subject", subject);
+
+    let { data: paper, error: paperError } = await db
+      .from("papers")
+      .select("id")
+      .eq("slug", path.split("/")[2])
+      .eq("subject_id", subject?.id)
+      .maybeSingle();
+    if (!level) {
+      // insert new level
+    }
+    // console.log("paper", paper);
+
+    // const { data, error } = await db.from("resources").insert({
+      level_id: level?.id,
+      subject_id: subject?.id,
+      paper_id: paper?.id,
+      ...resource,
+    });
+
+    // if (error) throw new Error(error.message);
 
     return NextResponse.json({ success: true }, { status: 201 });
   } catch (error) {

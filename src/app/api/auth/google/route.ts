dafad161a4490f2ba2f82f19f569ecb9a4c2b04b
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-20 08:30:01 +0530

    feat(auth): add Google OAuth initialization API route

---
+++ src/app/api/auth/google/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+
+  console.log(process.env.GOOGLE_CLIENT_ID);
+
+  const options = {
+    redirect_uri: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/google/callback`,
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+  };
+
+  const qs = new URLSearchParams(options);
+
+  return NextResponse.redirect(`${rootUrl}?${qs}`);
+}

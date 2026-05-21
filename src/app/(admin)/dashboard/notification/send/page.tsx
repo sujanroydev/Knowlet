f72e251d40ce5581204973b0cc67b219fc4d563c
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-05-21 22:15:29 +0530

    feat(admin): add 403 forbidden page and redirect unauthorized users instead of returning 404

--- src/app/(admin)/dashboard/notification/send/page.tsx
+++ src/app/(admin)/dashboard/notification/send/page.tsx
@@ -1,13 +1,13 @@
 import NotificationAdminPage from "@/components/dashboard/notification/notification-admin-page";
 import { verifyAdmin } from "@/lib/auth";
-import { notFound } from "next/navigation";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const cookieStore = await cookies();
   const admin = await verifyAdmin(cookieStore.get("token")?.value);
 
-  if (!admin) return notFound();
+  if (!admin) redirect("/forbidden");
 
   return <NotificationAdminPage />;
 }
--- src/app/(admin)/dashboard/resources/create/page.tsx
+++ src/app/(admin)/dashboard/resources/create/page.tsx
@@ -1,5 +1,13 @@
 import CreateResourcePage from "@/components/dashboard/resources/create-resource-page";
+import { verifyAdmin } from "@/lib/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const cookieStore = await cookies();
+  const admin = await verifyAdmin(cookieStore.get("token")?.value);
+
+  if (!admin) redirect("/forbidden");
+
   return <CreateResourcePage />;
 }

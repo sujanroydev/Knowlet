85ef338d466ee0b6eb68d9bec39f2450f5ae2109
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-06-06 22:38:55 +0530

    refactor(profile): remove RecentActivity component and uncomment related sections in profile page

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import ProfileCard from "@/components/profile/ProfileCard";
-import RecentActivity from "@/components/profile/RecentActivity";
 import SettingsPanel from "@/components/profile/SettingsPanel";
 import StatsSection from "@/components/profile/StatsSection";
 
@@ -40,9 +39,9 @@ export default function Profile() {
   return (
     <div className="max-w-3xl mx-auto p-4 space-y-6">
       <ProfileCard />
-      {/* <StatsSection /> */}
-      {/* <RecentActivity /> */}
-      {/* <SettingsPanel /> */}
+      <StatsSection />
+      <RecentActivity />
+      <SettingsPanel />
     </div>
   );
 }

958d9f69014f1c3ac050464b2b8d99040234a60b
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-06-06 12:39:32 +0530

    feat(resource): refactor ResourceEditor and related components for improved content handling and state management

--- src/components/dashboard/resources/html-editor.tsx
+++ src/components/dashboard/resources/html-editor.tsx
@@ -1,15 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function HtmlEditor({
-  rowHtml,
-  setRowHtml,
+  content,
+  setContent,
 }: {
-  rowHtml: string;
-  setRowHtml: (rowHtml: string) => void;
+  content?: string;
+  setContent: (rowContent: string) => void;
 }) {
+  const [rowContent, setRowContent] = useState("");
+
+  useEffect(() => {
+    if (rowContent) {
+      setContent(
+        rowContent
+          .replaceAll(/\[cite_start\]\s*/g, "")
+          .replaceAll(/\s*\[cite: \d+\]/g, "")
+          .replaceAll(/\s*\[cite: \d+(, \d+)*\]/g, ""),
+      );
+    }
+  }, [rowContent]);
+
+  useEffect(() => {
+    setRowContent(content || "");
+  }, [content]);
+
   return (
     <div className="p-5">
       <textarea
-        value={rowHtml}
-        onChange={(e) => setRowHtml(e.target.value)}
+        value={rowContent}
+        onChange={(e) => setContent(e.target.value)}
         spellCheck={false}
         className="h-[650px] w-full resize-none rounded-2xl border border-slate-300 bg-slate-950 p-5 font-mono text-sm text-slate-100 outline-none focus:border-blue-500"
       />

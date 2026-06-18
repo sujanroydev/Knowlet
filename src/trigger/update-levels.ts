7abccb1fde4780b21a83cf504acc8057e1e6916d
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-06-18 17:50:57 +0530

    feat(email): add educational details update reminder email template

--- src/trigger/update-levels.ts
+++ src/trigger/update-levels.ts
@@ -1,10 +1,22 @@
+import connectDb from "@/lib/db";
+import { sendEmailByUserId } from "@/services/email/send";
+import { educationalDetailsUpdateReminderTemplate } from "@/services/email/templates/educational-details-update-reminder";
 import { schedules } from "@trigger.dev/sdk/v3";
-import { sendNotificationByEmailId } from "@/services/notification/send";
 
 export const updateLevels = schedules.task({
   id: "update-levels",
-  cron: "0 0 1 1,7 *", // Jan 1 and Jul 1
+  cron: "0 0 1 1,7 *",
   run: async () => {
-    // recalculate levels
+    // const db = await connectDb();
+    // const { data, error } = await db.from("users").select("id");
+
+    // if (error) throw error;
+    // const users = data.map((i) => i.id) as string[];
+
+    await sendEmailByUserId({
+      user_id: "7cf87d0f-55d0-4275-93df-d240980e436c",
+      subject: "Verify your academic information",
+      html: educationalDetailsUpdateReminderTemplate(),
+    });
   },
 });

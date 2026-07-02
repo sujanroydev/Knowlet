26d50521736ec99017e47ffa7e3b7585efb55e51
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-07-02 23:11:23 +0530

    fix(editor): refactor HtmlEditor to streamline content handling and improve readability

--- src/components/dashboard/resources/html-editor.tsx
+++ src/components/dashboard/resources/html-editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { html as beautifyHtml } from "js-beautify";
 
 export default function HtmlEditor({
@@ -10,35 +10,31 @@ export default function HtmlEditor({
   content?: string;
   setContent: (rowContent: string) => void;
 }) {
-  const [rowContent, setRowContent] = useState(content || "");
+  function handleChange(
+    e: ChangeEvent<HTMLTextAreaElement, HTMLTextAreaElement>,
+  ): void {
+    const rowContent = e.target.value;
 
-  useEffect(() => {
-    if (rowContent) {
-      const cleanedContent = rowContent
-        .replaceAll(/\[cite_start\]\s*/g, "")
-        .replaceAll(/\s*\[cite: \d+\]/g, "")
-        .replaceAll(/\s*\[cite: \d+(, \d+)*\]/g, "");
+    const cleanedContent = rowContent
+      .replaceAll(/\[cite_start\]\s*/g, "")
+      .replaceAll(/\s*\[cite: \d+\]/g, "")
+      .replaceAll(/\s*\[cite: \d+(, \d+)*\]/g, "");
 
-      const beautifiedContent = beautifyHtml(cleanedContent, {
-        indent_size: 4,
-        wrap_line_length: 0,
-        preserve_newlines: true,
-        max_preserve_newlines: 1,
-      });
+    const beautifiedContent = beautifyHtml(cleanedContent, {
+      indent_size: 4,
+      wrap_line_length: 0,
+      preserve_newlines: true,
+      max_preserve_newlines: 1,
+    });
 
-      setContent(beautifiedContent);
-    }
-  }, [rowContent]);
-
-  useEffect(() => {
-    setRowContent(content || "");
-  }, [content]);
+    setContent(beautifiedContent);
+  }
 
   return (
     <div className="p-5">
       <textarea
-        value={rowContent}
-        onChange={(e) => setRowContent(e.target.value)}
+        value={content}
+        onChange={handleChange}
         spellCheck={false}
         className="h-[650px] w-full resize-none rounded-2xl border border-slate-300 bg-slate-950 p-5 font-mono text-sm text-slate-100 outline-none focus:border-blue-500"
       />

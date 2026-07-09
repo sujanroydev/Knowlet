ed085d73584365c7fc6ac126416506ebcf1374f2
Sujan Kumar Roy <mr.sujan.kumar.roy@gmail.com>
2026-07-09 10:51:04 +0530

    feat(mail): add metadata for MailPage component in dashboard

--- src/app/(admin)/dashboard/mail/page.tsx
+++ src/app/(admin)/dashboard/mail/page.tsx
@@ -1,4 +1,10 @@
 import MailPage from "@/components/dashboard/mail/MailPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mail | Dashboard | Knowlet",
+  description: "Compose and send emails to Knowlet users.",
+};
 
 function page() {
   return <MailPage />;
